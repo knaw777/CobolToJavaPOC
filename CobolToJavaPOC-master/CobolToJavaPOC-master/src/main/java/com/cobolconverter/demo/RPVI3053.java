84e647247847f92bee62ba861c4de18defabf90a
knaw777 <31465756+knaw777@users.noreply.github.com>
2017-12-28 17:42:30 +0530

    file modified for build
    file modified for build

---
+++ CobolToJavaPOC-master/CobolToJavaPOC-master/src/main/java/com/cobolconverter/demo/RPVI3053.java
@@ -0,0 +1,240 @@
+package com.cobolconverter.demo;
+
+
+
+
+public class RPVI3053 {
+	
+	public char ORB_HDR_TAPE;
+	public String ORB_CREATE_YYMMDD;
+	public String ORB_CREATE_HHMN;
+	
+	public Integer ORB_ITEM_NUM;
+	
+	public String ORB_CUST_ACCT_NUM;
+	public String ORB_PMT_DATE_YY;
+	public String ORB_PMT_DATE_MM;
+	public String ORB_PMT_DATE_DD;
+	public Float ORB_AMT_PAID;
+	public String ORB_TRACK_NUM_PRI;
+	public String ORB_TRACK_NUM_SEC;
+	public String ORB_PMT_TYPE;
+	public String ORB_NSF_INDR; 
+	public String ORB_NSF_REASON_CODE;
+	public String ORB_ProcessType;
+	public String ORB_PMT_CONFIRMATION_NO;
+	public String ORB_PMT_FILLER; 
+	public String ORB_V5INDR;
+	public String ORB_BANK_ID;
+	public String ORB_BANK_ACCT_NUM;
+	public String ORB_HDR_FILLER_81;
+	public String ORB_LRECL;
+	public String ORB_BLKSIZE;
+	public String ORB_HDR_FILLER_96;
+	public String ORB_TRANS_COUNT;
+	public Float ORB_TRANS_AMT;
+	public String ORB_TRL_TRANS_FILLER_87;
+	public Integer ORB_TAPE_COUNT; 
+	public Float ORB_TAPE_AMT;
+	public String ORB_TRL_TAPE_FILLER_87;
+	
+	
+	public char getORB_HDR_TAPE() {
+		return ORB_HDR_TAPE;
+	}
+	public void setORB_HDR_TAPE(char oRB_HDR_TAPE) {
+		ORB_HDR_TAPE = oRB_HDR_TAPE;
+	}
+	public String getORB_CREATE_YYMMDD() {
+		return ORB_CREATE_YYMMDD;
+	}
+	public void setORB_CREATE_YYMMDD(String oRB_CREATE_YYMMDD) {
+		ORB_CREATE_YYMMDD = oRB_CREATE_YYMMDD;
+	}
+	public String getORB_CREATE_HHMN() {
+		return ORB_CREATE_HHMN;
+	}
+	public void setORB_CREATE_HHMN(String oRB_CREATE_HHMN) {
+		ORB_CREATE_HHMN = oRB_CREATE_HHMN;
+	}
+	public Integer getORB_ITEM_NUM() {
+		return ORB_ITEM_NUM;
+	}
+	public void setORB_ITEM_NUM(Integer oRB_ITEM_NUM) {
+		ORB_ITEM_NUM = oRB_ITEM_NUM;
+	}
+	public String getORB_CUST_ACCT_NUM() {
+		return ORB_CUST_ACCT_NUM;
+	}
+	public void setORB_CUST_ACCT_NUM(String oRB_CUST_ACCT_NUM) {
+		ORB_CUST_ACCT_NUM = oRB_CUST_ACCT_NUM;
+	}
+	public String getORB_PMT_DATE_YY() {
+		return ORB_PMT_DATE_YY;
+	}
+	public void setORB_PMT_DATE_YY(String oRB_PMT_DATE_YY) {
+		ORB_PMT_DATE_YY = oRB_PMT_DATE_YY;
+	}
+	public String getORB_PMT_DATE_MM() {
+		return ORB_PMT_DATE_MM;
+	}
+	public void setORB_PMT_DATE_MM(String oRB_PMT_DATE_MM) {
+		ORB_PMT_DATE_MM = oRB_PMT_DATE_MM;
+	}
+	public String getORB_PMT_DATE_DD() {
+		return ORB_PMT_DATE_DD;
+	}
+	public void setORB_PMT_DATE_DD(String oRB_PMT_DATE_DD) {
+		ORB_PMT_DATE_DD = oRB_PMT_DATE_DD;
+	}
+	public Float getORB_AMT_PAID() {
+		return ORB_AMT_PAID;
+	}
+	public void setORB_AMT_PAID(Float oRB_AMT_PAID) {
+		ORB_AMT_PAID = oRB_AMT_PAID;
+	}
+	public String getORB_TRACK_NUM_PRI() {
+		return ORB_TRACK_NUM_PRI;
+	}
+	public void setORB_TRACK_NUM_PRI(String oRB_TRACK_NUM_PRI) {
+		ORB_TRACK_NUM_PRI = oRB_TRACK_NUM_PRI;
+	}
+	public String getORB_TRACK_NUM_SEC() {
+		return ORB_TRACK_NUM_SEC;
+	}
+	public void setORB_TRACK_NUM_SEC(String oRB_TRACK_NUM_SEC) {
+		ORB_TRACK_NUM_SEC = oRB_TRACK_NUM_SEC;
+	}
+	public String getORB_PMT_TYPE() {
+		return ORB_PMT_TYPE;
+	}
+	public void setORB_PMT_TYPE(String oRB_PMT_TYPE) {
+		ORB_PMT_TYPE = oRB_PMT_TYPE;
+	}
+	public String getORB_NSF_INDR() {
+		return ORB_NSF_INDR;
+	}
+	public void setORB_NSF_INDR(String oRB_NSF_INDR) {
+		ORB_NSF_INDR = oRB_NSF_INDR;
+	}
+	public String getORB_NSF_REASON_CODE() {
+		return ORB_NSF_REASON_CODE;
+	}
+	public void setORB_NSF_REASON_CODE(String oRB_NSF_REASON_CODE) {
+		ORB_NSF_REASON_CODE = oRB_NSF_REASON_CODE;
+	}
+	public String getORB_ProcessType() {
+		return ORB_ProcessType;
+	}
+	public void setORB_ProcessType(String oRB_ProcessType) {
+		ORB_ProcessType = oRB_ProcessType;
+	}
+	public String getORB_PMT_CONFIRMATION_NO() {
+		return ORB_PMT_CONFIRMATION_NO;
+	}
+	public void setORB_PMT_CONFIRMATION_NO(String oRB_PMT_CONFIRMATION_NO) {
+		ORB_PMT_CONFIRMATION_NO = oRB_PMT_CONFIRMATION_NO;
+	}
+	public String getORB_PMT_FILLER() {
+		return ORB_PMT_FILLER;
+	}
+	public void setORB_PMT_FILLER(String oRB_PMT_FILLER) {
+		ORB_PMT_FILLER = oRB_PMT_FILLER;
+	}
+	public String getORB_V5INDR() {
+		return ORB_V5INDR;
+	}
+	public void setORB_V5INDR(String oRB_V5INDR) {
+		ORB_V5INDR = oRB_V5INDR;
+	}
+	public String getORB_BANK_ID() {
+		return ORB_BANK_ID;
+	}
+	public void setORB_BANK_ID(String oRB_BANK_ID) {
+		ORB_BANK_ID = oRB_BANK_ID;
+	}
+	public String getORB_BANK_ACCT_NUM() {
+		return ORB_BANK_ACCT_NUM;
+	}
+	public void setORB_BANK_ACCT_NUM(String oRB_BANK_ACCT_NUM) {
+		ORB_BANK_ACCT_NUM = oRB_BANK_ACCT_NUM;
+	}
+	public String getORB_HDR_FILLER_81() {
+		return ORB_HDR_FILLER_81;
+	}
+	public void setORB_HDR_FILLER_81(String oRB_HDR_FILLER_81) {
+		ORB_HDR_FILLER_81 = oRB_HDR_FILLER_81;
+	}
+	public String getORB_LRECL() {
+		return ORB_LRECL;
+	}
+	public void setORB_LRECL(String oRB_LRECL) {
+		ORB_LRECL = oRB_LRECL;
+	}
+	public String getORB_BLKSIZE() {
+		return ORB_BLKSIZE;
+	}
+	public void setORB_BLKSIZE(String oRB_BLKSIZE) {
+		ORB_BLKSIZE = oRB_BLKSIZE;
+	}
+	public String getORB_HDR_FILLER_96() {
+		return ORB_HDR_FILLER_96;
+	}
+	public void setORB_HDR_FILLER_96(String oRB_HDR_FILLER_96) {
+		ORB_HDR_FILLER_96 = oRB_HDR_FILLER_96;
+	}
+	public String getORB_TRANS_COUNT() {
+		return ORB_TRANS_COUNT;
+	}
+	public void setORB_TRANS_COUNT(String oRB_TRANS_COUNT) {
+		ORB_TRANS_COUNT = oRB_TRANS_COUNT;
+	}
+	public Float getORB_TRANS_AMT() {
+		return ORB_TRANS_AMT;
+	}
+	public void setORB_TRANS_AMT(Float oRB_TRANS_AMT) {
+		ORB_TRANS_AMT = oRB_TRANS_AMT;
+	}
+	public String getORB_TRL_TRANS_FILLER_87() {
+		return ORB_TRL_TRANS_FILLER_87;
+	}
+	public void setORB_TRL_TRANS_FILLER_87(String oRB_TRL_TRANS_FILLER_87) {
+		ORB_TRL_TRANS_FILLER_87 = oRB_TRL_TRANS_FILLER_87;
+	}
+	public Integer getORB_TAPE_COUNT() {
+		return ORB_TAPE_COUNT;
+	}
+	public void setORB_TAPE_COUNT(Integer oRB_TAPE_COUNT) {
+		ORB_TAPE_COUNT = oRB_TAPE_COUNT;
+	}
+	public Float getORB_TAPE_AMT() {
+		return ORB_TAPE_AMT;
+	}
+	public void setORB_TAPE_AMT(Float oRB_TAPE_AMT) {
+		ORB_TAPE_AMT = oRB_TAPE_AMT;
+	}
+	public String getORB_TRL_TAPE_FILLER_87() {
+		return ORB_TRL_TAPE_FILLER_87;
+	}
+	public void setORB_TRL_TAPE_FILLER_87(String oRB_TRL_TAPE_FILLER_87) {
+		ORB_TRL_TAPE_FILLER_87 = oRB_TRL_TAPE_FILLER_87;
+	}
+	@Override
+	public String toString() {
+		return "RPVI3053 [ORB_HDR_TAPE=" + ORB_HDR_TAPE + ", ORB_CREATE_YYMMDD=" + ORB_CREATE_YYMMDD
+				+ ", ORB_CREATE_HHMN=" + ORB_CREATE_HHMN + ", ORB_ITEM_NUM=" + ORB_ITEM_NUM + ", ORB_CUST_ACCT_NUM="
+				+ ORB_CUST_ACCT_NUM + ", ORB_PMT_DATE_YY=" + ORB_PMT_DATE_YY + ", ORB_PMT_DATE_MM=" + ORB_PMT_DATE_MM
+				+ ", ORB_PMT_DATE_DD=" + ORB_PMT_DATE_DD + ", ORB_AMT_PAID=" + ORB_AMT_PAID + ", ORB_TRACK_NUM_PRI="
+				+ ORB_TRACK_NUM_PRI + ", ORB_TRACK_NUM_SEC=" + ORB_TRACK_NUM_SEC + ", ORB_PMT_TYPE=" + ORB_PMT_TYPE
+				+ ", ORB_NSF_INDR=" + ORB_NSF_INDR + ", ORB_NSF_REASON_CODE=" + ORB_NSF_REASON_CODE
+				+ ", ORB_ProcessType=" + ORB_ProcessType + ", ORB_PMT_CONFIRMATION_NO=" + ORB_PMT_CONFIRMATION_NO
+				+ ", ORB_PMT_FILLER=" + ORB_PMT_FILLER + ", ORB_V5INDR=" + ORB_V5INDR + ", ORB_BANK_ID=" + ORB_BANK_ID
+				+ ", ORB_BANK_ACCT_NUM=" + ORB_BANK_ACCT_NUM + ", ORB_HDR_FILLER_81=" + ORB_HDR_FILLER_81
+				+ ", ORB_LRECL=" + ORB_LRECL + ", ORB_BLKSIZE=" + ORB_BLKSIZE + ", ORB_HDR_FILLER_96="
+				+ ORB_HDR_FILLER_96 + ", ORB_TRANS_COUNT=" + ORB_TRANS_COUNT + ", ORB_TRANS_AMT=" + ORB_TRANS_AMT
+				+ ", ORB_TRL_TRANS_FILLER_87=" + ORB_TRL_TRANS_FILLER_87 + ", ORB_TAPE_COUNT=" + ORB_TAPE_COUNT
+				+ ", ORB_TAPE_AMT=" + ORB_TAPE_AMT + ", ORB_TRL_TAPE_FILLER_87=" + ORB_TRL_TAPE_FILLER_87 + "]";
+	}
+
+
+}
---
+++ CobolToJavaPOC-master/CobolToJavaPOC-master/src/main/java/com/cobolconverter/demo/RPVIPKEY.java
@@ -0,0 +1,101 @@
+package com.cobolconverter.demo;
+
+
+public class RPVIPKEY {
+	public String PAY_DB_TEL_LINE;
+	
+	public String PAY_DB_TEL_NPA;
+	public String PAY_DB_TEL_NXX;
+	public String PAY_DB_AUDIT_ID_PRI1;
+	public String PAY_DB_AUDIT_ID_PRI2;
+	public String PAY_DB_STATUS_YEAR;
+	public String PAY_DB_STATUS_MONTH;
+	public String PAY_DB_STATUS_DAY;
+	public String PAY_DB_STATUS_HOUR; 
+	public String PAY_DB_STATUS_MIN;
+	public String PAY_DB_STATUS_SEC;
+	public String PAY_DB_STATUS_MSEC;
+	public String getPAY_DB_TEL_LINE() {
+		return PAY_DB_TEL_LINE;
+	}
+	public void setPAY_DB_TEL_LINE(String pAY_DB_TEL_LINE) {
+		PAY_DB_TEL_LINE = pAY_DB_TEL_LINE;
+	}
+	public String getPAY_DB_TEL_NPA() {
+		return PAY_DB_TEL_NPA;
+	}
+	public void setPAY_DB_TEL_NPA(String pAY_DB_TEL_NPA) {
+		PAY_DB_TEL_NPA = pAY_DB_TEL_NPA;
+	}
+	public String getPAY_DB_TEL_NXX() {
+		return PAY_DB_TEL_NXX;
+	}
+	public void setPAY_DB_TEL_NXX(String pAY_DB_TEL_NXX) {
+		PAY_DB_TEL_NXX = pAY_DB_TEL_NXX;
+	}
+	public String getPAY_DB_AUDIT_ID_PRI1() {
+		return PAY_DB_AUDIT_ID_PRI1;
+	}
+	public void setPAY_DB_AUDIT_ID_PRI1(String pAY_DB_AUDIT_ID_PRI1) {
+		PAY_DB_AUDIT_ID_PRI1 = pAY_DB_AUDIT_ID_PRI1;
+	}
+	public String getPAY_DB_AUDIT_ID_PRI2() {
+		return PAY_DB_AUDIT_ID_PRI2;
+	}
+	public void setPAY_DB_AUDIT_ID_PRI2(String pAY_DB_AUDIT_ID_PRI2) {
+		PAY_DB_AUDIT_ID_PRI2 = pAY_DB_AUDIT_ID_PRI2;
+	}
+	public String getPAY_DB_STATUS_YEAR() {
+		return PAY_DB_STATUS_YEAR;
+	}
+	public void setPAY_DB_STATUS_YEAR(String pAY_DB_STATUS_YEAR) {
+		PAY_DB_STATUS_YEAR = pAY_DB_STATUS_YEAR;
+	}
+	public String getPAY_DB_STATUS_MONTH() {
+		return PAY_DB_STATUS_MONTH;
+	}
+	public void setPAY_DB_STATUS_MONTH(String pAY_DB_STATUS_MONTH) {
+		PAY_DB_STATUS_MONTH = pAY_DB_STATUS_MONTH;
+	}
+	public String getPAY_DB_STATUS_DAY() {
+		return PAY_DB_STATUS_DAY;
+	}
+	public void setPAY_DB_STATUS_DAY(String pAY_DB_STATUS_DAY) {
+		PAY_DB_STATUS_DAY = pAY_DB_STATUS_DAY;
+	}
+	public String getPAY_DB_STATUS_HOUR() {
+		return PAY_DB_STATUS_HOUR;
+	}
+	public void setPAY_DB_STATUS_HOUR(String pAY_DB_STATUS_HOUR) {
+		PAY_DB_STATUS_HOUR = pAY_DB_STATUS_HOUR;
+	}
+	public String getPAY_DB_STATUS_MIN() {
+		return PAY_DB_STATUS_MIN;
+	}
+	public void setPAY_DB_STATUS_MIN(String pAY_DB_STATUS_MIN) {
+		PAY_DB_STATUS_MIN = pAY_DB_STATUS_MIN;
+	}
+	public String getPAY_DB_STATUS_SEC() {
+		return PAY_DB_STATUS_SEC;
+	}
+	public void setPAY_DB_STATUS_SEC(String pAY_DB_STATUS_SEC) {
+		PAY_DB_STATUS_SEC = pAY_DB_STATUS_SEC;
+	}
+	public String getPAY_DB_STATUS_MSEC() {
+		return PAY_DB_STATUS_MSEC;
+	}
+	public void setPAY_DB_STATUS_MSEC(String pAY_DB_STATUS_MSEC) {
+		PAY_DB_STATUS_MSEC = pAY_DB_STATUS_MSEC;
+	} 
+	
+	@Override
+	public String toString() {
+		return "RPVIPKEY [PAY_DB_TEL_LINE=" + PAY_DB_TEL_LINE + ", PAY_DB_TEL_NPA=" + PAY_DB_TEL_NPA
+				+ ", PAY_DB_TEL_NXX=" + PAY_DB_TEL_NXX + ", PAY_DB_AUDIT_ID_PRI1=" + PAY_DB_AUDIT_ID_PRI1
+				+ ", PAY_DB_AUDIT_ID_PRI2=" + PAY_DB_AUDIT_ID_PRI2 + ", PAY_DB_STATUS_YEAR=" + PAY_DB_STATUS_YEAR
+				+ ", PAY_DB_STATUS_MONTH=" + PAY_DB_STATUS_MONTH + ", PAY_DB_STATUS_DAY=" + PAY_DB_STATUS_DAY
+				+ ", PAY_DB_STATUS_HOUR=" + PAY_DB_STATUS_HOUR + ", PAY_DB_STATUS_MIN=" + PAY_DB_STATUS_MIN
+				+ ", PAY_DB_STATUS_SEC=" + PAY_DB_STATUS_SEC + ", PAY_DB_STATUS_MSEC=" + PAY_DB_STATUS_MSEC + "]";
+	}
+
+}
