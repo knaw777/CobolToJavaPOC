d7041fe4e8c600617c6991cf118a7972191baff0
Khader <khadern@newtglobal.com>
2017-12-29 15:39:55 +0530

    File modified for final build

--- src/main/java/com/cobolconverter/demo/CobolToJavaConverter.java
+++ src/main/java/com/cobolconverter/demo/CobolToJavaConverter.java
@@ -28,6 +28,8 @@ public class CobolToJavaConverter {
 	
 
 	static int WS_TOTAL_CNT = 0;
+	static int WS_TOTAL_DTL=0;
+	static int WS_SAVE_ITEM=0;
 	static int WS_VZPP_ORB_PMT_ITEMS = 0;
 
 	static float WS_VZPP_ORB_PMT_AMT = 0;
@@ -35,18 +37,18 @@ public class CobolToJavaConverter {
 	static int WS_VZPP_ORB_RTN_ITEMS = 0;
 	static float WS_VZPP_ORB_RTN_AMT = 0;
 	final static DecimalFormat decimalFormat = new DecimalFormat("000000");
-
-	static String DATE_FORMAT = "yyyyMMdd";
+	final static DecimalFormat totadecimalFormat = new DecimalFormat("0000000000");
+	static String DATE_FORMAT = "yyMMdd";
 	static SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
 	static Calendar c1 = Calendar.getInstance(); // today
 	static Date date = new Date();
 	static DateFormat format = new SimpleDateFormat("HHmm");
 
-	private static String inputFile="D:\\files\\NTF4RPV_RPVF3053_G0325.txt";
+	private static String inputFile="E:\\NTF4RPV_RPVF3053_G0325.txt";
 	
-	private static String outputRPVFPO0A="D:\\files\\RPVFPO0A.txt";
+	private static String outputRPVFPO0A="E:\\RPVFPO0A.txt";
 	
-	private static String outputRPVFPO0Z="D:\\files\\RPVFPO0Z.txt";
+	private static String outputRPVFPO0Z="E:\\RPVFPO0Z.txt";
 
 	public static void convertCobolToJava() throws IOException {
 		
@@ -111,6 +113,7 @@ public class CobolToJavaConverter {
 		while (arrRPVI3Key.size() > j) {
 			RPVIPKEY rpvipkey = arrRPVI3Key.get(j);
 			RPVI3053 rpvi3053 = arrRPVI3053.get(j);
+			System.out.println(rpvi3053.toString());
 			if (j == 0) {
 				writeHeader(bw, rpvi3053);
 
@@ -149,6 +152,7 @@ public class CobolToJavaConverter {
 		 * ORBIT-POSTING-RECORD MOVE 000000 TO ORB-ITEM-NUM OF
 		 * ORBIT-POSTING-RECORD
 		 */
+		header.append(rpvi3053.getORB_HDR_TAPE());
 		header.append(sdf.format(c1.getTime()));
 		header.append(format.format(date));
 		rpvi3053.setORB_ITEM_NUM(0);
@@ -161,6 +165,7 @@ public class CobolToJavaConverter {
 		header.append("02010398101");
 		rpvi3053.setORB_BANK_ID("02010398101");
 		rpvi3053.setORB_BANK_ACCT_NUM("02010398101");
+		header.append("02010398101");
 		WS_TOTAL_CNT++;
 		bw.write(header.toString());
 		bw.newLine();
@@ -173,18 +178,20 @@ public class CobolToJavaConverter {
 		 * MOVE '0800' TO ORB-BLKSIZE OF ORBIT-POSTING-RECORD ADD 1 TO
 		 * WS-TOTAL-CNT WRITE RPVFPO0A-REC FROM ORBIT-POSTING-RECORD.
 		 */
-
-		header.append(sdf.format(c1.getTime()));
-		header.append(format.format(date));
-		header.append("02010398101");
+		header=null;
+		StringBuffer headertxn = new StringBuffer();
+		headertxn.append(rpvi3053.getORB_HDR_TAPE());
+		headertxn.append(sdf.format(c1.getTime()));
+		headertxn.append(format.format(date));
+		headertxn.append("000000");
 		rpvi3053.setORB_ITEM_NUM(0);
-		header.append(rpvi3053.getORB_ITEM_NUM());
 		rpvi3053.setORB_LRECL("080");
-		header.append(rpvi3053.getORB_LRECL());
+		headertxn.append(rpvi3053.getORB_LRECL());
 		rpvi3053.setORB_BLKSIZE("0800");
-		header.append(rpvi3053.getORB_BLKSIZE());
-		bw.write(header.toString());
+		headertxn.append(rpvi3053.getORB_BLKSIZE());
+		bw.write(headertxn.toString());
 		bw.newLine();
+		headertxn=null;
 	}
 
 	/**
@@ -229,6 +236,7 @@ public class CobolToJavaConverter {
 		 * WRITE RPVFPO0A-REC FROM ORBIT-POSTING-RECORD.
 		 */
 
+		sb.append(rpvi3053.getORB_HDR_TAPE());
 		sb.append(sdf.format(c1.getTime()));
 		sb.append(format.format(date));
 		sb.append(decimalFormat.format(i));
@@ -236,8 +244,8 @@ public class CobolToJavaConverter {
 		sb.append(rpvi3053.getORB_PMT_DATE_YY());
 		sb.append(rpvi3053.getORB_PMT_DATE_MM());
 		sb.append(rpvi3053.getORB_PMT_DATE_DD());
-
-		sb.append(rpvi3053.getORB_AMT_PAID().toString().replace(".", ""));
+		String amtPaid=rpvi3053.getORB_AMT_PAID().toString().replace(".", "");
+		sb.append(totadecimalFormat.format(rpvi3053.getORB_AMT_PAID()));
 		sb.append(rpvi3053.getORB_TRACK_NUM_PRI());
 		sb.append(rpvi3053.getORB_TRACK_NUM_SEC());
 		sb.append(rpvi3053.getORB_PMT_TYPE());
@@ -249,6 +257,9 @@ public class CobolToJavaConverter {
 		bw.write(sb.toString());
 		bw.newLine();
 		sb = null;
+		WS_TOTAL_DTL++;
+		WS_TOTAL_CNT++;
+		WS_SAVE_ITEM++;
 	}
 
 	/**
@@ -284,31 +295,32 @@ public class CobolToJavaConverter {
 		 */
 
 		WS_TOTAL_CNT++;
+		tailer.append(rpvi3053.getORB_HDR_TAPE());
 		tailer.append(sdf.format(c1.getTime()));
 		tailer.append(format.format(date));
 		rpvi3053.setORB_ITEM_NUM(999999);
 		tailer.append(rpvi3053.getORB_ITEM_NUM());
-		rpvi3053.setORB_TAPE_COUNT(WS_TOTAL_CNT);
-		tailer.append(WS_TOTAL_CNT);
+		rpvi3053.setORB_TAPE_COUNT(WS_TOTAL_DTL);
+		tailer.append(decimalFormat.format(WS_TOTAL_DTL));
 		rpvi3053.setORB_TAPE_AMT(WS_TOTAL_AMT);
-		tailer.append(String.format("%.2f", rpvi3053.getORB_TAPE_AMT()).replace(".", ""));
+		String totAmt=String.format("%.2f", rpvi3053.getORB_TAPE_AMT()).replace(".", "");
+		Integer temp=new Integer(totAmt);
+		tailer.append(totadecimalFormat.format(temp));
 		bw.write(tailer.toString());
+		bw.newLine();
 		WS_TOTAL_CNT++;
-		tailer.append(rpvi3053.getORB_ITEM_NUM());
-		tailer.append("02010398101");
-		rpvi3053.setORB_BANK_ID("02010398101");
-		rpvi3053.setORB_BANK_ACCT_NUM("02010398101");
-		WS_TOTAL_CNT++;
-		tailer.append(sdf.format(c1.getTime()));
-		tailer.append(format.format(date));
+		tailer.delete(0, tailer.length());
+		tailer.append(rpvi3053.getORB_HDR_TAPE());
 		tailer.append(sdf.format(c1.getTime()));
 		tailer.append(format.format(date));
 		rpvi3053.setORB_ITEM_NUM(999999);
 		tailer.append(rpvi3053.getORB_ITEM_NUM());
 		rpvi3053.setORB_TAPE_COUNT(WS_TOTAL_CNT);
-		tailer.append(WS_TOTAL_CNT);
+		tailer.append(decimalFormat.format(WS_TOTAL_CNT));
 		rpvi3053.setORB_TAPE_AMT(WS_TOTAL_AMT);
-		tailer.append(String.format("%.2f", rpvi3053.getORB_TAPE_AMT()).replace(".", ""));
+		totAmt=String.format("%.2f", rpvi3053.getORB_TAPE_AMT()).replace(".", "");
+		temp=new Integer(totAmt);
+		tailer.append(totadecimalFormat.format(temp));	
 		bw.write(tailer.toString());
 		bw.newLine();
 		tailer = null;
@@ -343,14 +355,14 @@ public class CobolToJavaConverter {
 		wORB_AMT_PAID = wORB_AMT_PAID + "." + dORB_AMT_PAID;
 
 		cpRPVI3053.setORB_AMT_PAID(Float.valueOf(wORB_AMT_PAID));
-		cpRPVI3053.setORB_TRACK_NUM_PRI(sCurrentLine.substring(100, 123));
-		cpRPVI3053.setORB_TRACK_NUM_SEC(sCurrentLine.substring(123, 128));
+		cpRPVI3053.setORB_TRACK_NUM_PRI(sCurrentLine.substring(110, 133));
+		cpRPVI3053.setORB_TRACK_NUM_SEC(sCurrentLine.substring(133, 138));
 
-		cpRPVI3053.setORB_PMT_TYPE(sCurrentLine.substring(128, 130));
-		cpRPVI3053.setORB_NSF_INDR(sCurrentLine.substring(130, 131));
-		cpRPVI3053.setORB_NSF_REASON_CODE(sCurrentLine.substring(131, 134));
-		cpRPVI3053.setORB_ProcessType(sCurrentLine.substring(134, 139));
-		cpRPVI3053.setORB_PMT_CONFIRMATION_NO(sCurrentLine.substring(139, 151));
+		cpRPVI3053.setORB_PMT_TYPE(sCurrentLine.substring(138, 140));
+		cpRPVI3053.setORB_NSF_INDR(sCurrentLine.substring(140, 141));
+		cpRPVI3053.setORB_NSF_REASON_CODE(sCurrentLine.substring(141, 144));
+		cpRPVI3053.setORB_ProcessType(sCurrentLine.substring(144, 149));
+		cpRPVI3053.setORB_PMT_CONFIRMATION_NO(sCurrentLine.substring(149, 161));
 		cpRPVI3053.setORB_V5INDR(sCurrentLine.substring(183, 184));
 
 		return cpRPVI3053;
@@ -388,6 +400,7 @@ public class CobolToJavaConverter {
 	private static void accumulateVZPPTotals(RPVI3053 rpvi3053) {
 		if (rpvi3053.getORB_NSF_REASON_CODE() == null || rpvi3053.getORB_NSF_REASON_CODE() == "") {
 			WS_VZPP_ORB_PMT_ITEMS++;
+			WS_TOTAL_DTL++;
 			WS_VZPP_ORB_PMT_AMT = WS_VZPP_ORB_PMT_AMT + rpvi3053.getORB_AMT_PAID();
 			WS_TOTAL_AMT = WS_TOTAL_AMT + rpvi3053.getORB_AMT_PAID();
 		} else {
